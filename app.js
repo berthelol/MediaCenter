f7ec3e7b325c8eb5d678ee6dddfbd276b8f70bf3
Berthelot Loïc <loic.berthelot1@gmail.com>
2016-11-03 01:05:13 +0100

    Creating a good skeleton
    used express generator

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
-
-
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
 
-app.get('/', function (req, res) {
-  //res.send('It Works!');             // one line response
-  res.sendfile('index.html');     // or send a webpage you designed
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.get('/toto', function (req, res) {
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  res.send('it\' working!!!');     // or send a webpage you designed
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-var server = app.listen(80,function () {
-
-  console.log('Node Express Webserver Started');
-
-});
-
-//console.log('Your friendly Express server, listening on port %s', server.address().port);
+module.exports = app;
