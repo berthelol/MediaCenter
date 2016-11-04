11aebd89f2a95ac546d0a1c49a2d0d532fd34e58
Berthelot Loïc <loic.berthelot1@gmail.com>
2016-11-04 15:00:22 +0100

    test route add /toto

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/toto', function(req, res, next) {
+  res.render('index', { title: 'Bigot Fuck you' });
+});
+
+
 module.exports = router;
