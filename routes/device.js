37323c26a9000652876ea4a1a76f6b42b61eb4b2
Berthelot Loïc <loic.berthelot1@gmail.com>
2016-11-04 15:59:06 +0100

    change routes

--- routes/device.js
+++ routes/device.js
@@ -13,6 +13,10 @@ router.get('/pH/bacplus', function(req, res, next) {
 router.get('/pH/data', function(req, res, next) {
   res.render('index', { title: 'Bigot Fuck you' });
 });
+
+router.get('/pH/historique', function(req, res, next) {
+  res.render('index', { title: 'Bigot Fuck you' });
+});
 //Chlore
 router.get('/chlore/bacmoins', function(req, res, next) {
   res.render('index', { title: 'Bigot Fuck you' });
@@ -21,22 +25,19 @@ router.get('/chlore/bacmoins', function(req, res, next) {
 router.get('/chlore/data', function(req, res, next) {
   res.render('index', { title: 'Bigot Fuck you' });
 });
+
+router.get('/chlore/historique', function(req, res, next) {
+  res.render('index', { title: 'Bigot Fuck you' });
+});
 //Temperature
 router.get('/temp/data', function(req, res, next) {
   res.render('index', { title: 'Bigot Fuck you' });
 });
-//historique
-router.get('/historique/pH', function(req, res, next) {
+
+router.get('/temp/historique', function(req, res, next) {
   res.render('index', { title: 'Bigot Fuck you' });
 });
 
-router.get('/historique/chlore', function(req, res, next) {
-  res.render('index', { title: 'Bigot Fuck you' });
-});
-
-router.get('/historique/temp', function(req, res, next) {
-  res.render('index', { title: 'Bigot Fuck you' });
-});
 //Parametres
 router.get('/parametres', function(req, res, next) {
   res.render('index', { title: 'Bigot Fuck you' });
