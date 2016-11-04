8a7a00c1b733b51d186ca6d4d9033b2c581decc7
Berthelot Loïc <loic.berthelot1@gmail.com>
2016-11-04 15:01:55 +0100

    test change /alex/bigot

--- routes/device.js
+++ routes/device.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/bigot', function(req, res, next) {
-  res.respond("Fuck you");
+  res.render('index', { title: 'Bigot Fuck you' });
 });
 
 module.exports = router;
