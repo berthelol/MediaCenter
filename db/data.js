a7c96d1daa25b60185b5e3b51b3107094ac4dca4
Berthelot Loïc <loic.berthelot1@gmail.com>
2016-11-08 01:35:20 +0100

    test

--- db/data.js
+++ db/data.js
@@ -1,9 +1,13 @@
 // import the necessary modules
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+// create an export function to encapsulate the model creation
+module.exports = function() {
   // define schema
   var DataSchema = new Schema({
     name: String,
-    age: Number
+    age: Number,
   });
   mongoose.model('Data', DataSchema);
+};
