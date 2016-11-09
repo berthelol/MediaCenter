4209405ac6d9c6e51f9a75ccc8caf46799d3cc9a
Berthelot Loïc <loic.berthelot1@gmail.com>
2016-11-09 17:06:32 +0100

    DC

--- db/user.js
+++ db/user.js
@@ -40,7 +40,7 @@ var App = function() {
     //find user
     this.find = function(callback) {
         User.findOne({}, function(err, user) {
-          if (err) return error(err, callback);
+          if (err) return callback(err,null); 
           if (user == null) return callback('No user found',null);
           callback(null, user);
         });
