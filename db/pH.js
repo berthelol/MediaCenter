507aa8185085ffd0ba3c26e4119ae44c0b2accee
Berthelot Loïc <loic.berthelot1@gmail.com>
2016-11-10 00:57:03 +0100

    ph last one historique add

--- db/pH.js
+++ db/pH.js
@@ -2,38 +2,86 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-  // define schema
-  var pHSchema = new Schema({
-    bac:{
-      bacmoins:
-      {
-        flag:{type:Boolean},
-        remplissage:{type:Number,min:0,max:100}
-      },
-      bacplus:
-      {
-        flag:{type:Boolean},
-        remplissage:{type:Number,min:0,max:100}
-      },
+// define schema
+var pHSchema = new Schema({
+    bac: {
+        bacmoins: {
+            flag: {
+                type: Boolean
+            },
+            remplissage: {
+                type: Number,
+                min: 0,
+                max: 100
+            }
+        },
+        bacplus: {
+            flag: {
+                type: Boolean
+            },
+            remplissage: {
+                type: Number,
+                min: 0,
+                max: 100
+            }
+        },
     },
-    data:{
-      time_of_mesure:{type:Date,default: Date.now()}
-      mesure:{type:Number,min:0,max:14}
+    data: {
+        time_of_mesure: {
+            type: Date,
+            default: Date.now()
+        }
+        mesure: {
+            type: Number,
+            min: 0,
+            max: 14
+        }
     }
-  });
-  var pH = mongoose.model('pH', pHSchema);
+});
+var pH = mongoose.model('pH', pHSchema);
 
 
 
-  var App = function() {
+var App = function() {
 
-  	var self = this;
-  	this.add = function(data, callback) {
-  		pH = new pH(data);
-  	  callback(pH);
-  	};
-  	this._Model = pH;
-  	this._Schema = pHSchema;
-  }
+    var self = this;
+    //add a data
+    this.add = function(datafrompost, callback) {
+        ph = new pH({
+            data.mesure = datafrompost.mesure
+        });
+        ph.save(function(err) {
+            if (err) {
+                return callback(err, null);
+            }
+            callback(null, ph);
+        });
+    };
+    //find all historique
+    this.findall = function(callback) {
+        pH.find({}, function(err, historique) {
+            var pHMap = {};
 
-  module.exports = new App();
+            pHMap.forEach(function(ph) {
+                pHMap[user._id] = ph;
+            });
+            callback(pHMap);
+        });
+    };
+    //find the last data
+    this.findlast = function(callback) {
+      pH.findOne({}, function(err, ph) {
+          if (err) {
+              callback(err, null);
+          } else if (ph == null) {
+              callback("No ph found", null)
+          } else {
+              callback(null, ph);
+          }
+      });
+    }
+    this._Model = pH;
+    this._Schema = pHSchema;
+}
+
+module.exports = new App();
--- routes/device.js
+++ routes/device.js
@@ -2,48 +2,87 @@ var express = require('express');
 var router = express.Router();
 //var path = require('path');
 // import the global schema, this can be done in any file that needs the model
-var Data = require('../db/data.js');
+var pH = require('../db/pH.js');
 
 
 //pH
 router.get('/pH/bacmoins', function(req, res, next) {
-  res.render('index', { title: 'Bigot Fuck you' });
+    res.render('index', {
+        title: 'Bigot Fuck you'
+    });
 });
 
 router.post('/pH/bacmoins', function(req, res, next) {
-  res.send(req.body.name);
+    res.render('index', {
+        title: 'Bigot Fuck you'
+    });
 });
 
 router.get('/pH/bacplus', function(req, res, next) {
-  res.render('index', { title: 'Bigot Fuck you' });
+    res.render('index', {
+        title: 'Bigot Fuck you'
+    });
 });
-
+//get last data
 router.get('/pH/data', function(req, res, next) {
-  res.render('index', { title: 'Bigot Fuck you' });
+    pH.findlast(function(ph)
+  {
+    res.status(200).json(historique);
+  });    
+});
+//add a new data
+router.post('/pH/data', function(req, res, next) {
+    var data = {
+        mesure: req.body.mesure
+    };
+    pH.add(data, function(err, ph) {
+        if (err) {
+            return res.status(500).json({
+                success: false,
+                msg: err.msg
+            });
+        }
+        res.status(200).json({
+            success: true,
+            msg: 'Successful added pH mesure'
+        });
+    });
 });
 
 router.get('/pH/historique', function(req, res, next) {
-  res.render('index', { title: 'Bigot Fuck you' });
+    pH.findall(function(historique) {
+        res.status(200).json(historique);
+    });
 });
 //Chlore
 router.get('/chlore/bac', function(req, res, next) {
-  res.render('index', { title: 'Bigot Fuck you' });
+    res.render('index', {
+        title: 'Bigot Fuck you'
+    });
 });
 
 router.get('/chlore/data', function(req, res, next) {
-  res.render('index', { title: 'Bigot Fuck you' });
+    res.render('index', {
+        title: 'Bigot Fuck you'
+    });
 });
 
 router.get('/chlore/historique', function(req, res, next) {
-  res.render('index', { title: 'Bigot Fuck you' });
+    res.render('index', {
+        title: 'Bigot Fuck you'
+    });
 });
 //Temperature
 router.get('/temp/data', function(req, res, next) {
-  res.render('index', { title: 'Bigot Fuck you' });
+    res.render('index', {
+        title: 'Bigot Fuck you'
+    });
 });
 
 router.get('/temp/historique', function(req, res, next) {
-  res.render('index', { title: 'Bigot Fuck you' });
+    res.render('index', {
+        title: 'Bigot Fuck you'
+    });
 });
 
 //Parametres
@@ -52,11 +91,11 @@ router.get('/parametres', function(req, res, next) {
 });
 //test
 router.get('/test/:toto', function(req, res, next) {
-  //get from url which user we want
+    //get from url which user we want
     var condition = {
         "data": req.params.toto,
     };
-  res.send('Tu a ecrit: '+condition.data);
+    res.send('Tu a ecrit: ' + condition.data);
 });
 
 module.exports = router;
