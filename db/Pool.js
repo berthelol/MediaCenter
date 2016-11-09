5bf459e404ffa5dad0abfe1825d54d010c3c72ad
Berthelot Loïc <loic.berthelot1@gmail.com>
2016-11-09 16:08:25 +0100

    add cannot add another pool

--- db/Pool.js
+++ db/Pool.js
@@ -3,40 +3,47 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 //Define a schema
 var PoolSchema = new Schema({
-        size: {
-            type: Number
-        },
-        type: {
-            type: String
-        },
-        heated: {
-            type: Boolean
-        }
+    size: {
+        type: Number
+    },
+    type: {
+        type: String
+    },
+    heated: {
+        type: Boolean
+    }
 });
-  var Pool = mongoose.model('Pool', PoolSchema);
+var Pool = mongoose.model('Pool', PoolSchema);
 
-  var App = function() {
+var App = function() {
 
-  	var self = this;
+    var self = this;
     this.add = function(data, callback) {
-        pool = new Pool(data);
-        pool.save(function(err) {
-            if(err) return error(err, callback);
-            callback(null, pool);
+        self.find(function(err, pool) {
+            //no pool exist
+            if (err) {
+                pool = new Pool(data);
+                pool.save(function(err) {
+                    if (err) return error(err, callback);
+                    callback(null, pool);
+                });
+            } else {
+                return error('Pool already exist', callback);
+            }
         });
 
-    };
-  	
-    this.find = function(callback)
-    {
-      Pool.findOne({}, function(err, pool) {
-          if(err) return error(err, callback);
-          if(pool == null) return error('No pool Found', callback);
 
-          callback(null, pool);
-      });
+    };
+
+    this.find = function(callback) {
+        Pool.findOne({}, function(err, pool) {
+            if (err) return error(err, callback);
+            if (pool == null) return error('No pool Found', callback);
+
+            callback(null, pool);
+        });
     }
-  	this._Model = Pool;
-  	this._Schema = PoolSchema;
-  }
-  module.exports = new App();
+    this._Model = Pool;
+    this._Schema = PoolSchema;
+}
+module.exports = new App();
