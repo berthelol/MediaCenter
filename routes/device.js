9f2f8e1cd953745acae90f62b8bb8ea18e2c048b
Berthelot Loïc <loic.berthelot1@gmail.com>
2016-11-11 01:34:21 +0100

    test

--- routes/device.js
+++ routes/device.js
@@ -82,7 +82,7 @@ router.get('/chlore/bac', function(req, res, next) {
 });
 //add a new data
 router.post('/chlore/data', function(req, res, next) {
-  console.log(req.body.mesure);
+  console.log(req.body);
 
   var data = {
       mesure: req.body.mesure,
