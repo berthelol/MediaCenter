f6df34b5a75d21e3bf29a8f88128f9bfde4f3365
Berthelot Loïc <loic.berthelot1@gmail.com>
2016-11-12 02:24:32 +0100

    test require schema

--- routes/order.js
+++ routes/order.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var Order = require('../db/Order.js');
+var OrderSchema =require('mongoose').model('Order').schema;
 //add a new order
 router.post('/', function(req, res, next) {
-  if(Order.model("Order").schema.path('bac.which_bac').enumValues.indexOf(req.body.bac.which_bac) > -1)
+  if(OrderSchema.path('bac.which_bac').enumValues.indexOf(req.body.bac.which_bac) > -1)
   {
     console.log("no enum corresponded");
   }
