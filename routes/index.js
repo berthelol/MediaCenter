f87346ae6769327e380ee60f73a4148edff0123b
Berthelot Loïc <loic.berthelot1@gmail.com>
2016-11-23 13:30:15 +0100

    test routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/jeece', function(req, res, next) {
+router.get('jeece', function(req, res, next) {
     res.render('layout', { title: 'Velib' });
 });
 module.exports = router;
